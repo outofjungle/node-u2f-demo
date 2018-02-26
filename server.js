4b954270cc2cab6dce8ced8fe6e96ffe04c4dead
Venkat Venkataraju <ven@yubico.com>
2018-02-25 16:18:57 -0800

    cleaned up, mvp without verbose mode implemented

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const Fs = require('fs')
 const Database = require('./lib/database')
 const Device = require('./lib/device')
 
-var OPTS = {
+var serverOpts = {
   host: 'localhost',
   port: 4443,
   tls: {
@@ -20,39 +20,52 @@ var OPTS = {
   }
 }
 
-var APP_ID = ''
+var appId = ''
 if (process.env.GOOGLE_CLOUD_PROJECT) {
-  APP_ID = 'https://' + process.env.GOOGLE_CLOUD_PROJECT + '.appspot.com'
-  OPTS.port = process.env.PORT
-  delete OPTS.host
-  delete OPTS.tls
+  appId = 'https://' + process.env.GOOGLE_CLOUD_PROJECT + '.appspot.com'
+  serverOpts.port = process.env.PORT
+  delete serverOpts.host
+  delete serverOpts.tls
 } else {
-  APP_ID = 'https://' + OPTS.host + ':' + OPTS.port
+  appId = 'https://' + serverOpts.host + ':' + serverOpts.port
 }
 
-const server = Hapi.server(OPTS)
+const server = Hapi.server(serverOpts)
 const db = Database.dbInit(':memory:')
 
 process.on('SIGINT', function () {
-  server.stop({ timeout: 10000 }).then(function (err) {
+  server.stop({
+    timeout: 10000
+  }).then(function (err) {
     db.close()
     process.exit((err) ? 1 : 0)
   })
 })
 
 const provision = async () => {
-  let options = {
-    storeBlank: false,
-    cookieOptions: {
-      password: 'INSECUREINSECUREINSECUREINSECUREINSECUREINSECURE',
-      isSecure: true
-    }
-  }
-
   await server.register(require('inert'))
   await server.register({
+    plugin: require('good'),
+    options: {
+      reporters: {
+        console: [{
+            module: 'good-console'
+          },
+          'stdout'
+        ]
+      }
+    }
+  })
+
+  await server.register({
     plugin: require('yar'),
-    options: options
+    options: {
+      storeBlank: false,
+      cookieOptions: {
+        password: Math.random().toString(36) + Math.random().toString(36) + Math.random().toString(36),
+        isSecure: true
+      }
+    }
   })
 
   server.route({
@@ -88,7 +101,7 @@ const provision = async () => {
   server.route({
     method: 'GET',
     path: '/devices/{device}/register',
-    handler: Device.registerRequest(db, APP_ID)
+    handler: Device.registerRequest(db, appId)
   })
 
   server.route({
@@ -100,7 +113,7 @@ const provision = async () => {
   server.route({
     method: 'GET',
     path: '/devices/{device}/auth',
-    handler: Device.authRequest(db, APP_ID)
+    handler: Device.authRequest(db, appId)
   })
 
   server.route({
